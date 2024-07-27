3483751c63889e86cdf3fb3acf17f4ac876de7ba
Shahov Artem <shahov9ad@gmail.com>
2024-07-27 21:21:22 +0300

    smt

--- app/controllers/perfect_controller.rb
+++ app/controllers/perfect_controller.rb
@@ -3,5 +3,13 @@ class PerfectController < ApplicationController
   end
 
   def result
+    @numbers = params[:numbers].split(',').map(&:to_i)
+    @result = find_perfect_numbers(numbers)
+  end
+
+  private 
+
+  def find_perfect_numbers(numbers)
+    numbers
   end
 end
